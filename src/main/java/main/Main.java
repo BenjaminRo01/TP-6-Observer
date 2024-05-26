77b4bbe34b29abc2739fd76752a6ec95785bf894
BenjaminRo01 <benjaroselo01@gmail.com>
2024-05-26 15:07:39 -0300

    Update Main.java

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -1,5 +1,6 @@
 package main;
 
+import model.Medidor;
 import model.WeatherChannelService;
 
 public class Main {
@@ -8,9 +9,8 @@ public class Main {
         String lon = "-62.9947";
         String unidad = "metric"; //Celsius
         String url = "https://api.openweathermap.org/data/2.5/weather?lat=" + lat + "&lon=" + lon + "&appid=d2038c709af19ed6bb5d8b191708bdaa&units=" + unidad;
-        WeatherChannelService weather = new WeatherChannelService(url);
-        System.out.println(weather.temperatura());
-
+        Medidor medidor = new Medidor(new WeatherChannelService(url));
+        System.out.println(medidor.leerTemperatura());
 
 
 
